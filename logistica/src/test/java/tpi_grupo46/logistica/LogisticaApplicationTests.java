c27909362eb881283fba87d0c6295163d1310ab4
Camilo Martin Valentino <camilomartinv789@gmail.com>
2025-11-06 18:39:35 -0300

    Restart

---
+++ logistica/src/test/java/tpi_grupo46/logistica/LogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package tpi_grupo46.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ recursos/src/main/java/tpi_grupo46/recursos/RecursosApplication.java
@@ -0,0 +1,13 @@
+package tpi_grupo46.recursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecursosApplication.class, args);
+	}
+
+}
